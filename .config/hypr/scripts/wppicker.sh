de55961adbb10f99cd81464d440729124ddf9899
Bintang M <bintangtesto@gmail.com>
2025-07-21 14:23:23 +0700

    Add files via upload

--- .config/hypr/scripts/wppicker.sh
+++ .config/hypr/scripts/wppicker.sh
@@ -6,8 +6,11 @@ SYMLINK_PATH="$HOME/.config/hypr/current_wallpaper"
 
 cd "$WALLPAPER_DIR" || exit 1
 
-# === ICON-PREVIEW SELECTION WITH ROFI ===
-SELECTED_WALL=$(for a in *.jpg *.png; do echo -en "$a\0icon\x1f$a\n" ; done | rofi -dmenu -p "")
+# === handle spaces name
+IFS=$'\n'
+
+# === ICON-PREVIEW SELECTION WITH ROFI, SORTED BY NEWEST ===
+SELECTED_WALL=$(for a in $(ls -t *.jpg *.png *.gif 2>/dev/null); do echo -en "$a\0icon\x1f$a\n"; done | rofi -dmenu -p "")
 [ -z "$SELECTED_WALL" ] && exit 1
 SELECTED_PATH="$WALLPAPER_DIR/$SELECTED_WALL"
 
@@ -17,3 +20,4 @@ matugen image "$SELECTED_PATH"
 # === CREATE SYMLINK ===
 mkdir -p "$(dirname "$SYMLINK_PATH")"
 ln -sf "$SELECTED_PATH" "$SYMLINK_PATH"
+
